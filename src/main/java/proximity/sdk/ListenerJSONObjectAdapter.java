cac6d5d7ef3c39dabfc421695eb4a014a994b71e
Pavel Movchan <ukrolove@gmail.com>
2017-12-31 20:24:06 +0200

    AA: request a session token automatically

--- src/main/java/proximity/sdk/ListenerJSONObjectAdapter.java
+++ src/main/java/proximity/sdk/ListenerJSONObjectAdapter.java
@@ -3,15 +3,31 @@ package proximity.sdk;
 import org.json.JSONObject;
 import postman.Client;
 import postman.response.Response;
+import proximity.sdk.entity.RefreshToken;
+import proximity.sdk.entity.Session;
 import proximity.sdk.exception.HttpException;
 import proximity.sdk.exception.UnathenticatedErrorException;
 import proximity.sdk.exception.ValidatorErrorException;
 
 abstract public class ListenerJSONObjectAdapter implements Client.Listener<JSONObject> {
     private SDK.ErrorCallback errorCallback;
+    private SDK sdk;
+    private Boolean tryUpdatingSessionToken = true;
 
-    ListenerJSONObjectAdapter(SDK.ErrorCallback errorCallback) {
+    /**
+     * @param sdk {@link SDK}
+     * @param errorCallback {@link SDK.ErrorCallback}
+     */
+    ListenerJSONObjectAdapter(SDK sdk, SDK.ErrorCallback errorCallback) {
+        this.sdk = sdk;
         this.errorCallback = errorCallback;
+    }
+
+    /**
+     * @param tryUpdatingSessionToken whether the listener should try to request a new session token
+     */
+    public void setTryUpdatingSessionToken(Boolean tryUpdatingSessionToken) {
+        this.tryUpdatingSessionToken = tryUpdatingSessionToken;
     }
 
     /**
@@ -45,11 +61,45 @@ abstract public class ListenerJSONObjectAdapter implements Client.Listener<JSONO
     }
 
     /**
-     * User implementation of 2xx code
+     * Client implementation of 2xx code
      *
      * @param response is a server response
      */
     abstract public void handleSuccess(Response<JSONObject> response);
+
+    /**
+     * Requests a new session token with a refresh token
+     */
+    private void requestNewSessionToken() {
+        sdk.renewSessionToken(sdk.getRefreshToken(), new SDK.SessionTokenRenewalCallback() {
+            @Override
+            public void renewed(Session session, RefreshToken refreshToken) {
+                sdk.setRefreshToken(refreshToken.getToken());
+                sdk.setSessionToken(session.getToken());
+
+                sdk.getOnTokenUpdateCallback().renewed(session, refreshToken);
+
+                sessionTokenUpdated();
+            }
+        }, new SDK.ErrorCallback() {
+            @Override
+            public void http(HttpException exception) {
+                errorCallback.http(exception);
+            }
+
+            @Override
+            public void runtime(Throwable exception) {
+                errorCallback.runtime(exception);
+            }
+        });
+    }
+
+    /**
+     * Invoked after an automatic session token renewal
+     */
+    public void sessionTokenUpdated() {
+
+    }
 
     /**
      * 4xx or 5xx
@@ -65,6 +115,11 @@ abstract public class ListenerJSONObjectAdapter implements Client.Listener<JSONO
             if (isValidationError(response)) {
                 exception = new ValidatorErrorException(errorCode, response);
             } else if (isUnauthenticatedError(response)) {
+                if (sdk.shouldUpdateTokenAutomatically() && tryUpdatingSessionToken) {
+                    this.requestNewSessionToken();
+                    return;
+                }
+
                 exception = new UnathenticatedErrorException(errorCode, response);
             } else {
                 exception = new HttpException(errorCode, response);
