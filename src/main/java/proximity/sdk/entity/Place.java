2d5aef95b44bb427a5b19b9c0569ff37fa23dd03
Pavel Movchan <ukrolove@gmail.com>
2018-02-15 11:50:23 +0200

    Make the proximity.sdk.entity.Place class stringable

--- src/main/java/proximity/sdk/entity/Place.java
+++ src/main/java/proximity/sdk/entity/Place.java
@@ -1,11 +1,32 @@
 package proximity.sdk.entity;
 
 public class Place {
+    /**
+     * Name of the place.
+     */
     private String name;
+
+    /**
+     * Address of the place.
+     */
     private String address;
+
+    /**
+     * Google place id.
+     */
     private String placeId;
+
+    /**
+     * Coordinates of the place.
+     */
     private Location location;
 
+    /**
+     * @param name name of the place
+     * @param address address of the place
+     * @param placeId google place id
+     * @param location coordinates
+     */
     public Place(String name, String address, String placeId, Location location) {
         this.name = name;
         this.address = address;
@@ -13,19 +34,36 @@ public class Place {
         this.location = location;
     }
 
+    /**
+     * Name of the place.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Address of the place.
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Google place id.
+     */
     public String getPlaceId() {
         return placeId;
     }
 
+    /**
+     * Coordinates of the place.
+     */
     public Location getLocation() {
         return location;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
     }
 }
